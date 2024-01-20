7d771037f216e7cbd9463d037305564dbe00b442
Jason Smith <jason.inet@gmail.com>
2024-01-20 13:35:02 -0800

    Add basic implementations for default drive

--- src/main/java/frc/robot/commands/DefaultDriveCommand.java
+++ src/main/java/frc/robot/commands/DefaultDriveCommand.java
@@ -4,29 +4,68 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+import java.util.function.DoubleSupplier;
+import frc.robot.Constants;
 
 public class DefaultDriveCommand extends Command {
-  /** Creates a new DefaultDriveCommand. */
-  public DefaultDriveCommand() {
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
+    private final DrivetrainSubsystem m_drivetrainSubsystem;
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+    private final DoubleSupplier m_translationXSupplier;
+    private final DoubleSupplier m_translationYSupplier;
+    private final DoubleSupplier m_rotationSupplier;
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
+    private SlewRateLimiter RateLimiter_X;
+    private SlewRateLimiter RateLimiter_Y;
+    private SlewRateLimiter RateLimiter_R;
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+    private Boolean m_fieldRelative;
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+    public DefaultDriveCommand(DrivetrainSubsystem drivetrainSubsystem, Boolean fieldRelative,
+            DoubleSupplier translationXSupplier, DoubleSupplier translationYSupplier,
+            DoubleSupplier rotationSupplier) {
+        this.m_drivetrainSubsystem = drivetrainSubsystem;
+        this.m_translationXSupplier = translationXSupplier;
+        this.m_translationYSupplier = translationYSupplier;
+        this.m_rotationSupplier = rotationSupplier;
+        this.m_fieldRelative = fieldRelative;
+
+        addRequirements(drivetrainSubsystem);
+
+        RateLimiter_X = new SlewRateLimiter(DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND / Constants.DRIVE_TRANSLATION_RAMP_TIME);
+        RateLimiter_Y = new SlewRateLimiter(DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND / Constants.DRIVE_TRANSLATION_RAMP_TIME);
+        RateLimiter_R = new SlewRateLimiter(DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND / Constants.DRIVE_ROTATION_RAMP_TIME);
+    }
+
+    @Override
+    public void execute() {
+        // You can use `new ChassisSpeeds(...)` for robot-oriented movement instead of
+        // field-oriented movement
+
+        double x = RateLimiter_X.calculate(m_translationXSupplier.getAsDouble());
+        double y = RateLimiter_Y.calculate(m_translationYSupplier.getAsDouble());
+        double r = RateLimiter_R.calculate(m_rotationSupplier.getAsDouble());
+
+        if (m_fieldRelative) {
+            m_drivetrainSubsystem.drive(ChassisSpeeds.fromFieldRelativeSpeeds(
+                    x, y, r, m_drivetrainSubsystem.getGyroscopeRotation()));
+        } else {
+            m_drivetrainSubsystem.drive(new ChassisSpeeds(x, y, r));
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_drivetrainSubsystem.drive(new ChassisSpeeds(0.0, 0.0, 0.0));
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+      return false;
+    }
 }
